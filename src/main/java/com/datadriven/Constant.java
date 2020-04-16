d49ef074fa0f0dad180cb099be92cc2ab5887318
tonywilliams95 <40881448+tonywilliams95@users.noreply.github.com>
2020-04-16 21:22:42 +0530

    testdata5

--- src/main/java/com/datadriven/Constant.java
+++ src/main/java/com/datadriven/Constant.java
@@ -24,7 +24,7 @@ public class Constant {
 			+ "TestData" + File.separator;
 
 	public static final String File_TestData = "testdata.xlsx";
-	public static final String File_TestData3 = "testdata.xlsx";
+	public static final String File_TestData5 = "testdata.xlsx";
 
 
 }
